3a739dbb0a2b7e52dbb148dadcdcca9b4296e391
jaykhoyani127351-max <jay.khoyani127351@marwadiuniversity.ac.in>
2026-06-10 13:04:26 +0530

    Add files via upload

---
+++ Program1.php
@@ -0,0 +1,16 @@
+<?php
+
+    const university="MU";
+    const sem = 3;
+    $result = "PASS";
+    $name = "jay";
+    $cgpa = 8.6;
+
+    echo "University:".university."</br>";
+    echo "Name of Student:".$name."</br>";
+    echo "Semester:".sem."</br>";
+    echo "Result:".$result."</br>";
+    echo "Cgpa:".$cgpa."</br>";
+    
+
+?>
