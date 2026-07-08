7e037557be67f2d115cfe8d0439d378f7199612d
jaykhoyani127351-max <jay.khoyani127351@marwadiuniversity.ac.in>
2026-07-08 10:47:52 +0530

    Add files via upload

---
+++ 2.4program.php
@@ -0,0 +1,22 @@
+<?php
+
+$str = "Hello World, Welcome to PHP Programming!";
+
+echo $str . "<br><br>";
+
+
+echo strlen($str) . "<br><br>";
+
+echo strpos($str, "Welcome") . "<br><br>";
+
+echo str_word_count($str) . "<br><br>";
+
+
+echo strrev($str) . "<br><br>";
+
+
+echo  strtolower($str) . "<br><br>";
+
+
+echo  strtoupper($str) . "<br><br>";
+?>
